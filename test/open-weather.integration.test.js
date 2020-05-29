364cf0a8a9ce7f7b99c03587c1b73738786822f7
sandeepkumar <sandeep.kumar@flamingo.ai>
2020-05-30 08:24:43 +1000

    Update the integration tests to reflect the changes related to the function arguments and return types

--- test/open-weather.integration.test.js
+++ test/open-weather.integration.test.js
@@ -5,34 +5,37 @@ const openWeather = new OpenWeather();
 const conf = require('../configuration/open-weather');
 
 describe('OpenWeather', () => {
-    it('should return five day forecast when the getFiveDayForecast function is valid with valid key and city code', async () => {
-        let forecasts = await openWeather.getFiveDayForecast(conf.key, conf.sydneyCode);
-        let random = Math.floor(Math.random() * forecasts.length);
-        expect(forecasts).toBeInstanceOf(Array);
-        expect(forecasts).toBeDefined();
-        expect(forecasts.length).toBeGreaterThan(0);
-        expect(forecasts[random].main).toBeInstanceOf(Object);
-        expect(forecasts[random].main.temp_min).not.toBe('');
-        expect(forecasts[random].dt_txt).not.toBe('');
+    it('should return an Object containing status and five day forecast when the getFiveDayForecast function is valid with valid key and city code', async () => {
+        let forecastsData = await openWeather.getFiveDayForecast(conf.key, conf.sydneyCode);
+        let random = Math.floor(Math.random() * forecastsData.forecasts.length);
+        expect(forecastsData.forecasts).toBeInstanceOf(Array);
+        expect(forecastsData.forecasts).toBeDefined();
+        expect(forecastsData.forecasts.length).toBeGreaterThan(0);
+        expect(forecastsData.forecasts[random].main).toBeInstanceOf(Object);
+        expect(forecastsData.forecasts[random].main.temp_min).not.toBe('');
+        expect(forecastsData.forecasts[random].dt_txt).not.toBe('');
+        expect(forecastsData.status).toBe('success');
     });
 
-   it('should return an empty array when an invalid key is passed to getFiveDayForecast function', async () => {
-        let forecasts = await openWeather.getFiveDayForecast('', conf.sydneyCode);
-
-        expect(forecasts).toBeInstanceOf(Array);
-        expect(forecasts).toHaveLength(0);
+   it('should return an Object with error status and empty array when an invalid key is passed to getFiveDayForecast function', async () => {
+        let forecastsData = await openWeather.getFiveDayForecast('', conf.sydneyCode);
+        expect(forecastsData.status).toBe('error');
+        expect(forecastsData.forecasts).toBeInstanceOf(Array);
+        expect(forecastsData.forecasts).toHaveLength(0);
     });
 
-   it('should return an empty array when an invalid city code is passed ', async () => {
-       let forecasts = await openWeather.getFiveDayForecast(conf.key, '');
+   it('should return an Object with error status and empty array when an invalid city code is passed ', async () => {
+       let forecastsData = await openWeather.getFiveDayForecast(conf.key, '');
 
-       expect(forecasts).toBeInstanceOf(Array);
-       expect(forecasts).toHaveLength(0);
+       expect(forecastsData.forecasts).toBeInstanceOf(Array);
+       expect(forecastsData.forecasts).toHaveLength(0);
+       expect(forecastsData.status).toBe('error');
    });
 
-   it('should handle the exception and return an empty array when an error occurs in getFiveDayForecast function', async () => {
-       let forecasts = await openWeather.getFiveDayForecast('', '');
-       expect(forecasts).toBeInstanceOf(Array);
-       expect(forecasts).toHaveLength(0);
+   it('should return an Object with error status and empty array when exception occurs in getFiveDayForecast', async () => {
+       let forecastsData = await openWeather.getFiveDayForecast('', '');
+       expect(forecastsData.forecasts).toBeInstanceOf(Array);
+       expect(forecastsData.forecasts).toHaveLength(0);
+       expect(forecastsData.status).toBe('error');
    });
 });
