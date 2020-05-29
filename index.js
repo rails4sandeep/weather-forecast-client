97f5a4d5a7269b125d5806b1b0c331980530a1db
sandeepkumar <sandeep.kumar@flamingo.ai>
2020-05-29 11:31:57 +1000

    Move the open weather class function calls to an aync function

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const OpenWeather = require('./core/open-weather');
 const openWeather = new OpenWeather();
 
 let getSunnyWeatherSummary = async () => {
-    const forecasts = await openWeather.getFiveDayForecast(config.sydneyCode);
-    const sunnyDays = openWeather.getSunnyDays(forecasts);
-    const summary = openWeather.summarizeWeatherForecast(sunnyDays);
-    console.log(summary);
+    try {
+        const forecasts = await openWeather.getFiveDayForecast(config.key, config.sydneyCode);
+        const sunnyDays = openWeather.getSunnyDays(forecasts);
+        const summary = openWeather.summarizeWeatherForecast(sunnyDays);
+        console.log(summary);
+    } catch (e) {
+        console.log(e.message);
+    }
 };
 
 getSunnyWeatherSummary();
