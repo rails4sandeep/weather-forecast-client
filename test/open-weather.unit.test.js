d6aa9d93cfcff87c2dfc14249c2382a5a977bb54
sandeepkumar <sandeep.kumar@flamingo.ai>
2020-05-30 08:26:42 +1000

    Update the unit tests in relation to the open weather class function arguments and return types

--- test/open-weather.unit.test.js
+++ test/open-weather.unit.test.js
@@ -14,14 +14,17 @@ const forecastsSample = require('./samples/forecasts');
 
 describe('OpenWeather class', () => {
 
-    it('should return an array of sunny days when getSunnyDays is called', async () => {
+    it('should return an Object containing an status and array of sunny days when getSunnyDays is called', async () => {
         openWeather.getFiveDayForecast = jest.fn();
         openWeather.getFiveDayForecast.mockResolvedValueOnce(forecastsSample);
 
-        let forecasts = await openWeather.getFiveDayForecast(conf.key, conf.sydneyCode);
-        let sunnyDays = openWeather.getSunnyDays(forecasts);
+        let forecastsData = await openWeather.getFiveDayForecast(conf.key, conf.sydneyCode);
+        let sunnyDays = openWeather.getSunnyDays(forecastsData);
         expect(sunnyDays).toBeDefined();
-        expect(sunnyDays).toBeInstanceOf(Array);
+        expect(sunnyDays).toBeInstanceOf(Object);
+        expect(sunnyDays.status).toBe('success');
+        expect(sunnyDays.days).toBeInstanceOf(Array);
+        expect(sunnyDays.days.length).toBeGreaterThanOrEqual(0);
     });
 
     it('should return an array cleaning up the duplicates when removeDuplicates is called', async () => {
@@ -47,22 +50,29 @@ describe('OpenWeather class', () => {
     });
 
     it('should return an object having a message and an array of dates when summarizeWeatherForecast is called', async () => {
-        let sunnyDays = ['29-05-2020', '30-05-2020', '31-05-2020', '01-06-2020', '02-06-2020'];
+        let sunnyDays = {
+            status: "success",
+            days: ['29-05-2020', '30-05-2020', '31-05-2020', '01-06-2020', '02-06-2020']
+        };
         let summary = openWeather.summarizeWeatherForecast(sunnyDays);
 
         expect(summary).toBeInstanceOf(Object);
         expect(summary).toBeDefined();
-        expect(summary.message).not.toBeNaN();
+        expect(summary.message).toBe('Number of sunny days in the next five days forecast in Sydney: 5');
         expect(summary.sunnyDays).toBeInstanceOf(Array);
+        expect(summary.sunnyDays.length).toBe(5);
     });
 
-    it('should return empty array if getSunnyDays is called with empty forecasts', async () => {
-        let forecasts = [];
-        let sunnyDays = openWeather.getSunnyDays(forecasts);
-
-
-        expect(sunnyDays).toBeInstanceOf(Array);
-        expect(sunnyDays).toHaveLength(0);
+    it('should return an object containing a message and empty array if getSunnyDays is called with empty forecasts', async () => {
+        let forecastsData = {
+            status: "error",
+            forecasts: []
+        };
+        let sunnyDays = openWeather.getSunnyDays(forecastsData);
+        expect(sunnyDays).toBeInstanceOf(Object);
+        expect(sunnyDays.status).toBe('error');
+        expect(sunnyDays.days).toBeInstanceOf(Array);
+        expect(sunnyDays.days).toHaveLength(0);
     });
 
     it('should return empty array if removeDuplicates is called with empty days', async () => {
@@ -76,7 +86,6 @@ describe('OpenWeather class', () => {
     it('should return an empty array if setToFiveDayForecast is called with empty days', async () => {
         let days = [];
         let fiveDayForecast = openWeather.setToFiveDayForecast(days);
-
         expect(fiveDayForecast).toBeInstanceOf(Array);
         expect(fiveDayForecast).toHaveLength(0);
     });
@@ -89,13 +98,15 @@ describe('OpenWeather class', () => {
         expect(auFormatForecasts).toHaveLength(0);
     });
 
-    it('should return an object with just the message when summarizeWeatherForecast is called with empty sunnyDays', async () => {
-        let sunnyDays = [];
-        let summary = openWeather.summarizeWeatherForecast(sunnyDays);
+    it('should return an object with just the message when summarizeWeatherForecast with status as error', async () => {
+        let sunnyData = {
+            status: 'error',
+            days: []
+        };
+        let summary = openWeather.summarizeWeatherForecast(sunnyData);
 
         expect(summary).toBeInstanceOf(Object);
-        expect(summary.message).toBeDefined();
-        expect(summary.message).toBe('There are no sunny days in the next five day forecast');
+        expect(summary.message).toBe('An error occured communicating with open weather api');
         expect(summary.sunnyDays).not.toBeDefined();
     });
 
