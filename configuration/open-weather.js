2ae67b3c2bf2f6ed55289c9f20a60c3055cc0b08
sandeepkumar <sandeep.kumar@flamingo.ai>
2020-05-29 11:31:25 +1000

    Move some more configuration information to the open-weather config file

--- configuration/open-weather.js
+++ configuration/open-weather.js
@@ -4,6 +4,8 @@ let openWeatherConfig = {
   key: process.env.OPEN_WEATHER_KEY || '',
   host: 'https://api.openweathermap.org/',
   apiTimeout: '20000',
-  sydneyCode: "2147714"
+  sydneyCode: '2147714',
+  kelvinToCelsius: 273.15,
+  sunny: 20
 };
 module.exports = openWeatherConfig;
